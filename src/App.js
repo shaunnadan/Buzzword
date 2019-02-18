a148b2b4fc570fb1e95f335844f2b6e190e5e0f9
Brian Bancroft <hello@brianbancroft.ca>
2019-02-18 13:10:06 -0500

    ➕  Use prettier and helmet

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { ThemeProvider } from 'emotion-theming'
+import { Helmet } from 'react-helmet'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <ThemeProvider>
+        <Helmet>
+          <title>Buzzword Bingo Card Generator</title>
+          <meta
+            name="description"
+            content="It's time for some Buzzword Bingo!"
+          />
+          <meta name="theme-color" content="#008f68" />
+        </Helmet>
+        <div className="App">
+          <header className="App-header" />
+        </div>
+      </ThemeProvider>
+    )
   }
 }
 
-export default App;
+export default App
